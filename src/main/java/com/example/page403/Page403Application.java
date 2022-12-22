46c9877b5e52fddc431ca2df13f4a4246014d00e
Andre Ray <andreray053@gmail.com>
2022-12-21 23:11:27 -0300

    first commit

---
+++ src/main/java/com/example/page403/Page403Application.java
@@ -0,0 +1,13 @@
+package com.example.page403;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Page403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Page403Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/page403/Page403ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.page403;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Page403ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
